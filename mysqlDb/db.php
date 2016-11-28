adc5457a4bd28242bdd25206885d161a3ae6ff5e
paulappz <you@example.com>
2016-11-28 12:22:13 +0100

    dp params changed

--- mysqlDb/db.php
+++ mysqlDb/db.php
@@ -2,12 +2,13 @@
 
 /* Database config */
 
-$db_host		= 'localhost';
-$db_user		= 'root';
-$db_pass		= '';
-$db_database	= 'ondoconnect'; 
+$db_host		= getenv('DB_HOSTNAME');
+$db_user		= getenv('DB_USER');
+$db_pass		= getenv('DB_PASS');
+$db_database	= getenv('DB_SCHEMA'); 
 
 /* End config */
+
 
 
 
